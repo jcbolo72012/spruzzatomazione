33bba5ff89fe238896d79547e7b1bf122bdef17c
jcbolo72012 <jcbolo72012@gmail.com>
2024-05-24 01:18:47 -0400

    images

--- spa/watering/views.py
+++ spa/watering/views.py
@@ -1,6 +1,34 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+from .models import Question
 
 
 def index(request):
-    return HttpResponse("Hi Ruby Hi Hi Hi Hi i made it work yippee!")
+    # latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    # output = ", ".join([q.question_text for q in latest_question_list])
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    context = {"latest_question_list": latest_question_list}
+    return render(request, "watering/index.html", context)
+    # latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    # template = loader.get_template("polls/index.html")
+    # context = {
+    #     "latest_question_list": latest_question_list,
+    # }
+    # return HttpResponse(template.render(context, request))
+    # return HttpResponse(output)
+    # return HttpResponse("Hi Ruby Hi Hi Hi Hi i made it work yippee!")
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
+
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
+
+
